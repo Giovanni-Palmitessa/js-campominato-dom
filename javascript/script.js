6dd51ad1554379273d09c4de7f9dea45c9a26ed2
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-04-26 11:31:30 +0200

    add empty array

--- javascript/script.js
+++ javascript/script.js
@@ -9,41 +9,9 @@ let mainTitle = document.querySelector('.hidden');
 
 let eleButton = document.querySelector('#play');
 
+let randomNumbers = []
+
 mainTitle.innerHTML = 'Seleziona il livello di difficoltà e genera la griglia';
-
-/* DA QUI CODICE GIUSTO
-
-eleButton.addEventListener('click', function () {
-    mainTitle.innerHTML = '';
-
-    createGrid (100);
-
-    const listCells = document.querySelectorAll('.cell');
-
-    for (let i = 0; i < listCells.length; i++) {
-        const cell = listCells[i];
-        cell.addEventListener('click', function () {
-            this.classList.toggle('clicked');
-        }) 
-     }
-     
-     for (let i = 1; i <= 100; i++) {
-        console.log(i);
-     }
-})
-
-// functions
-
-function createGrid(numCells) {
-    eleGrid.innerHTML = '';
-    for (let i = 0; i < numCells; i++) {
-
-        eleGrid.innerHTML += `<div class="cell">${i + 1}<div>`
-    }
-}
-
-*/
-
 
 eleButton.addEventListener('click', function () {
     const eleDifficulty = document.getElementById("difficulty");
@@ -83,7 +51,11 @@ eleButton.addEventListener('click', function () {
 
 function createGrid(numCells) {
     eleGrid.innerHTML = '';
+
+    const arrPresentRandoms = [];
     for (let i = 0; i < numCells; i++) {
+
+        
 
         eleGrid.innerHTML += `<div class="cell">${i + 1}<div>`
     }
